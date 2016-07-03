c99c4ed77fb64ea7f9fbbf354c97154405ea736d
fzxt <fahadahmad41@hotmail.com>
2016-07-03 16:38:39 -0400

    add parser class

---
+++ api/parser/parser.py
@@ -0,0 +1,35 @@
+import discord
+import asyncio
+import parser_class
+
+
+client = discord.Client()
+token = 'MTk4OTMwNDMyNDg0NTczMTg0.Clr70w.SgQV-SK9VKYOFgFkC1pK3QyXwP0'
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+
+
+    help_dir_channel  = None
+    for server in client.servers:
+        for channel in server.channels:
+            if "helpdirectory" in channel.name:
+                help_dir_channel = channel
+
+    result = []
+    async for message in client.logs_from(help_dir_channel):
+              temp = { 'message': message.content, 'user': message.author.name}
+              result.append(temp)
+
+    MyParserClass = parser_class.ParserClass(result)
+    results = MyParserClass.check()
+    print(results)
+
+
+
+
+
+
+client.run(token)
---
+++ api/parser/parser_class.py
@@ -0,0 +1,22 @@
+class ParserClass():
+
+    def __init__(self, data):
+        self.data = data
+        self.terms = ['C++', 'C#', 'Python', 'SQL', 'HTML', 'CSS', 'Perl', 'JavaScript', 'Mongo']
+
+    def check(self):
+        """check if term (languages) are in messages"""
+        users = []
+
+        for data in self.data:
+            result = { 'membership': []}
+            user = data["user"]
+            message = data["message"]
+
+            for term in self.terms:
+                if term in message:
+                    result["user"] = user
+                    result["membership"].append(term)
+            users.append(result)
+
+        return users
