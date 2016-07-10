60a494a5c14ce444fa56bc3f955355c5badc508b
Mike Tung <miketung2013@gmail.com>
2016-07-10 12:25:32 -0400

    added constants and configs

---
+++ api/terms.py
@@ -0,0 +1,12 @@
+"""
+Terms File to load in to parser_class.py
+"""
+
+terms = [
+            'C++', 'C#', 'Python', 
+            'SQL', 'HTML', 'CSS', 
+            'Perl', 'JavaScript', 'Mongo',
+            'Java', 'LaTex', 'Splunk',
+            'Rust', 'SQL', 'R', 'ASP.NET',
+            'Groovy', 'Ruby', '.NET',
+            ]
